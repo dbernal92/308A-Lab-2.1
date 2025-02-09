6649cc67e87a527f9dedbc1a11c05b05cde85195
Darien Bernal <darien.bernal@gmail.com>
2025-02-09 00:22:05 -0500

    Created loop that logs each item in Robin's inventory

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const adventurer = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"]
+}
+
+// Loop that logs each item in Robin’s inventory
+for (let stat in adventurer) {
+    if (Array.isArray(adventurer[stat])) {
+        console.log(`${stat}:`);
+        for (let weapon of adventurer[stat]) {
+            console.log(`  ${weapon}`);
+        }
     }
+}
 
