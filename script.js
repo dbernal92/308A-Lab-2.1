d63b394dedeb7576b2ee0d8d931d12e361e76694
Darien Bernal <darien.bernal@gmail.com>
2025-02-10 00:03:27 -0500

    Created Character class, added dice roll function to class after it was created

--- script.js
+++ script.js
@@ -41,3 +41,34 @@ adventurer.roll = function(mod = 0) {
 
 adventurer.roll();
 
+// Part 2: Class Fantasy
+// Character class defines generic character entities
+class Character {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+}
+
+Character.prototype.roll = function(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+};
+
+// We can re-create Robin using the Character class
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+// Part 3: Class Features
+
+class Adventurer extends Character {
+    constructor (name, role) {
+        super(name);
+    }
+}
