2aad43ec6e9f9149bfa647f9e1cc339f8dad22c4
Darien Bernal <darien.bernal@gmail.com>
2025-02-09 23:45:39 -0500

    Added object within object, added array to newest object, created dice roll function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Part 1: Humble Beginnings
+//  A simple adventurer with basic properties such as health and an inventory
 const adventurer = {
     name: "Robin",
     health: 10,
@@ -13,4 +15,29 @@ for (let stat in adventurer) {
         }
     }
 }
+
+// Give Robin a companion to travel with
+adventurer.companion = {
+    name: "Leo",
+    type: "Cat"
+}
+
+console.log(adventurer);
+
+// Give Robin’s feline friend a friend of his own
+adventurer.companion.companion = {
+    name: "Frank",
+    type: "Flea",
+    // Has its own belongings, which includes a small hat and sunglasses
+    belongings: ["small hat", "sunglasses"]
+}
+
+console.log(adventurer.companion.companion);
+
+adventurer.roll = function(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+};
+
+adventurer.roll();
 
